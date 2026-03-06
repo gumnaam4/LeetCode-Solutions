2b9c14fafbfcedd6125c2d6609580e8110cd8c33
gumnaam4 <deoaayush4@gmail.com>
2026-03-07 03:55:09 +0530

    solved

---
+++ 13.roman-to-integer.py
@@ -0,0 +1,27 @@
+#
+# @lc app=leetcode id=13 lang=python3
+#
+# [13] Roman to Integer
+#
+
+# @lc code=start
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        values = {
+            'I':1, 'V':5, 'X':10,
+            'L':50, 'C':100,
+            'D':500, 'M':1000
+        }
+        
+        ans = 0
+        
+        for i in range(len(s)):
+            if i < len(s)-1 and values[s[i]] < values[s[i+1]]:
+                ans -= values[s[i]]
+            else:
+                ans += values[s[i]]
+                
+        return ans
+        
+# @lc code=end
+
