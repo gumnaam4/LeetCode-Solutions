75a573e4c342734be1714dfb62454ecbbb500d6d
gumnaam4 <deoaayush4@gmail.com>
2026-08-25 23:29:21 +0530

    final 3718

--- 3718.py
+++ 3718.py
@@ -1,4 +1,16 @@
-nums = [2,4,5,6,9]
+nums = [3,6,9,12,15,18]
 k = 3
-#for i in range(len(nums)):
-print(len(nums))
+l=0
+b=True
+for i in range(len(nums)):
+    if nums[i] % k == 0:
+        if l < nums[i]:
+            l = nums[i]
+#print(l//k)
+for j in range(1,l//k):
+    if j * k not in nums:
+        print(j * k)
+        b= False
+        break
+if b == True:
+    print(k * ((l//k)+1))          
